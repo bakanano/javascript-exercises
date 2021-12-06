2aceedf00b896eaf71b7e49c1686375ed6a3d9b9
bakanano <narutoz0rs@live.com>
2021-12-06 15:23:40 +1300

    Finished exercise 05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    if (args[0] < 0 || args[1] < 0 || 
+        typeof args[0] !== "number" || typeof args[1] !== "number") {
+            return "ERROR";
+    }
 
-};
+    args.sort((a, b) => {
+        return a - b;
+    })
+
+    let min = args[0];
+    let max = args[1];
+    return max * ((max + 1) / 2) - (((min - 1) * min) / 2);
+}
 
 // Do not edit below this line
 module.exports = sumAll;
