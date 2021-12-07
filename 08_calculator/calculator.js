9aadf8e54feedf5169a48801843177d469aeda7d
bakanano <narutoz0rs@live.com>
2021-12-07 15:11:29 +1300

    Finished exercise 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
+const add = function(x, y) {
+  return x + y;
+};
+
+const subtract = function(x, y) {
+  return x - y;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+  return array.reduce((a, b) => a +b, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
+};
+
+const power = function(x, y) {
+  return Math.pow(x, y);
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(n) {
+  return n == 0 ? 1 : n * factorial(n-1);
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
