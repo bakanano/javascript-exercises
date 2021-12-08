7b9281dc481d563bf068813c0a5a024415f6a41a
bakanano <narutoz0rs@live.com>
2021-12-08 17:49:22 +1300

    Finished exercise 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    let f = new Array(n+2);
+    let i;
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i <= n; i++) {
+        f[i] = f[i-1] + f[i-2];
+    }
+    return f[n];
 
 };
 
