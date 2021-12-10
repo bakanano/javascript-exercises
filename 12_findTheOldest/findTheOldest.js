9ab100703e2cb3ffb18aea653b66b8d53f941d76
bakanano <narutoz0rs@live.com>
2021-12-10 14:19:05 +1300

    Finished exercise 12 Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => getAge(b) - getAge(a)); //desc ordering, biggest -> smallest
+    return people[0];
+}
 
-};
+function getAge(person) {
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    return new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
